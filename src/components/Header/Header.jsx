39aeb511fe6cb145c16ab313e3c4b04a04cbba3c
Roy <roy.mattar@outlook.com>
2023-05-31 23:34:14 +0300

    Responsive design/CSS components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import Container from "react-bootstrap/Container";
+import "./Header.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 function Header() {
   return (
-    <div>
-      <h1>Little Lemon</h1>
-      <h2>Chicago</h2>
-      <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist </p>
-      <button>Reserve a Table</button>
-      <img src='./icons_assets/restauranfood.jpg' width={300} height={400}/>
-    </div>
-  )
+    <Container fluid id="container">
+      <Container>
+        <Row>
+          <Col>
+            <Row>
+              <h1 id="herotitle">Little Lemon</h1>
+            </Row>
+            <Row>
+              <h2 id="location">Chicago</h2>
+            </Row>
+            <Row>
+              <p id="herotext">
+                We are a family owned Mediterranean restaurant, focused on
+                traditional recipes served with a modern twist{" "}
+              </p>
+            </Row>
+            <Row>
+              <button id="rsrvbtn">Reserve a Table</button>
+            </Row>
+          </Col>
+          <Col>
+            <div className="imgcontainer">
+              <Image
+                src="./icons_assets/restauranfood.jpg"
+                width={360}
+                height={470}
+                id="heroimg"
+                fluid
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -1,21 +1,37 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./Nav.css";
 
 export default function Navigation() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home"><img src="Logo.svg"/></Navbar.Brand>
+    <Navbar expand="lg">
+      <Container id="navcontainer">
+        <Navbar.Brand href="#home" className="brand-left">
+          <img src="Logo.svg" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Menu</Nav.Link>
-            <Nav.Link href="#link">Reservation</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className="nav-link">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#link" className="nav-link">
+              About
+            </Nav.Link>
+            <Nav.Link href="#link" className="nav-link">
+              Menu
+            </Nav.Link>
+            <Nav.Link href="#link" className="nav-link">
+              Reservation
+            </Nav.Link>
+            <Nav.Link href="#link" className="nav-link">
+              Order Online
+            </Nav.Link>
+            <Nav.Link href="#link" className="nav-link">
+              Login
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
